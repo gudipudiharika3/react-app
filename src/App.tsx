7d0a0b25373640853165428fe566a444a2f2ff75
gudipudiharika3 <71378652+gudipudiharika3@users.noreply.github.com>
2024-06-09 15:55:36 -0500

    button component

--- src/App.tsx
+++ src/App.tsx
@@ -14,13 +14,30 @@ function App() {
     </div>
   );
 }*/
-import Alert from "./components/Alert";
+
+/*import Alert from "./components/Alert";
 function App() {
   return (
     <div>
       <Alert>
         Hello <span> World </span>
       </Alert>
+    </div>
+  );
+}*/
+import { useState } from "react";
+import Alert from "./components/Alert";
+import Button from "./components/Button";
+function App() {
+  const [alertVisible, setAlertVisibility] = useState(false);
+  return (
+    <div>
+      {alertVisible && (
+        <Alert onClose={() => setAlertVisibility(false)}>My Alert</Alert>
+      )}
+      <Button color="secondary" onClick={() => setAlertVisibility(true)}>
+        My Button
+      </Button>
     </div>
   );
 }
